2b9669d8e4a855fb3a139d583c1606c343fa7ae5
etincopa <81591906+etincopa@users.noreply.github.com>
2024-09-07 02:28:52 -0500

    mobile

---
+++ mobile-pesaje-produccion/www/appRoutes.js
@@ -0,0 +1,12 @@
+var mobile_appRoutes = [
+    {
+        "path": "/srv_api",
+        "destination": "CP_SRV",
+        "originDestination": "CP_SRV"
+    },
+    {
+        "path": "/saperp",
+        "destination": "S4H_HTTP_BASIC_CP",
+        "originDestination": "S4H_HTTP_BASIC_CP"
+    }
+];
